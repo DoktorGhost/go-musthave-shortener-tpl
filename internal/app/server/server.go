952700a7877e6065cc751f007195f2ae9ee641b6
Oleg Samsonov <ghostkote@gmail.com>
2024-05-03 16:14:43 +0300

    iter4

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -5,15 +5,16 @@ import (
 	"github.com/DoktorGhost/go-musthave-shortener-tpl/internal/app/storage/maps"
 	"github.com/DoktorGhost/go-musthave-shortener-tpl/internal/app/usecase"
 	"net/http"
+	"strconv"
 )
 
-func StartServer(port string) error {
+func StartServer(port int) error {
 	db := maps.NewMapStorage()
 	shortURLUseCase := usecase.NewShortURLUseCase(db)
 
 	r := handlers.InitRoutes(*shortURLUseCase)
 
-	err := http.ListenAndServe(":"+port, r)
+	err := http.ListenAndServe(":"+strconv.Itoa(port), r)
 
 	if err != nil {
 		return err
