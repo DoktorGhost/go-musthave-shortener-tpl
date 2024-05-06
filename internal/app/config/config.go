8133ca8df1402ede6fa65d2d80eb98de9c29b3b1
Oleg Samsonov <ghostkote@gmail.com>
2024-05-06 13:40:06 +0300

    iter5

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -3,11 +3,17 @@ package config
 import (
 	"flag"
 	"fmt"
+	"github.com/caarlos0/env/v6"
 	"strconv"
 	"strings"
 )
 
 // Config структура для хранения конфигурационных параметров/
+type HostPort struct {
+	Hp      []string `env:"SERVER_ADDRESS" envSeparator:":"`
+	BaseURL string   `env:"BASE_URL"`
+}
+
 type Config struct {
 	Host string
 	Port int
@@ -35,17 +41,36 @@ func (c *Config) Set(value string) error {
 var BaseURL string
 
 func ParseConfig() *Config {
+	var cfg HostPort
+	env.Parse(&cfg)
+	fmt.Println(cfg)
+
 	addr := new(Config)
 	_ = flag.Value(addr)
 	flag.Var(addr, "a", "Net address host:port")
-	flag.StringVar(&BaseURL, "b", "", "Net address host:port")
+	flag.StringVar(&BaseURL, "b", "", "Net address base url")
 	flag.Parse()
 
-	if addr.Host == "" {
-		addr.Host = "localhost"
+	if len(cfg.Hp) == 0 {
+		if addr.Host == "" {
+			addr.Host = "localhost"
+			fmt.Println("defaulting to localhost:", addr.Host)
+		}
+		if addr.Port == 0 {
+			addr.Port = 8080
+			fmt.Println("defaulting to 8080:", addr.Port)
+		}
+	} else {
+		addr.Host = cfg.Hp[0]
+		port, err := strconv.Atoi(cfg.Hp[1])
+		if err != nil {
+			return nil
+		}
+		addr.Port = port
 	}
-	if addr.Port == 0 {
-		addr.Port = 8080
+	if cfg.BaseURL != "" {
+		BaseURL = strings.TrimSuffix(cfg.BaseURL, "/")
 	}
+
 	return addr
 }
